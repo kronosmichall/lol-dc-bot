0afda34eda62a313a8e293f76481c952c8af1889
kronosmichall <kronosmichall@gmail.com>
2024-02-27 10:08:37 +0100

    s

--- index.js
+++ index.js
@@ -50,14 +50,14 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 })
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('hello im a dc bot');
-});
+  res.send('hello im a dc bot')
+})
 
-const port = 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
