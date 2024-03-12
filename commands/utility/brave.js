c4a912fc93d21b2336d810d3628a39cbe15b03a2
kronosmichall <kronosmichall@gmail.com>
2024-03-12 11:22:31 +0100

    try

--- commands/utility/brave.js
+++ commands/utility/brave.js
@@ -8,7 +8,12 @@ async function getScreenshot() {
     const page = await browser.newPage()
     await page.setViewport({ width: 1000, height: 800 })
     await page.goto('https://www.ultimate-bravery.net/Classic', { waitUntil: ['load', 'domcontentloaded'] })
-    await page.click('.fc-button.fc-cta-do-not-consent.fc-secondary-button')
+    try {
+        await page.click('.fc-button.fc-cta-do-not-consent.fc-secondary-button')
+    } catch (e) {
+        console.error('No cookie consent found')
+        console.error(e)
+    }
     await page.screenshot({ path: filename })
     await browser.close()
 
