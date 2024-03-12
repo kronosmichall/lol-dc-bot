1f5c977b378e90cf5a034624001fa20170f5a0f2
kronosmichall <kronosmichall@gmail.com>
2024-03-12 11:12:30 +0100

    no sandbox

--- commands/utility/brave.js
+++ commands/utility/brave.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 async function getScreenshot() {
     const filename = new Date().getTime() + '.png'
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
     const page = await browser.newPage()
     await page.setViewport({ width: 1000, height: 800 })
     await page.goto('https://www.ultimate-bravery.net/Classic')
