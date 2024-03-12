e5702080595a109d0e8b1e996435b066de421f69
kronosmichall <kronosmichall@gmail.com>
2024-03-12 11:19:02 +0100

    wait until pup

--- commands/utility/brave.js
+++ commands/utility/brave.js
@@ -7,7 +7,7 @@ async function getScreenshot() {
     const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
     const page = await browser.newPage()
     await page.setViewport({ width: 1000, height: 800 })
-    await page.goto('https://www.ultimate-bravery.net/Classic')
+    await page.goto('https://www.ultimate-bravery.net/Classic', { waitUntil: ['load', 'domcontentloaded'] })
     await page.click('.fc-button.fc-cta-do-not-consent.fc-secondary-button')
     await page.screenshot({ path: filename })
     await browser.close()
--- pup.js
+++ pup.js
@@ -3,10 +3,17 @@ const fs = require('fs')
 
 async function getScreenshot() {
     const filename = new Date().getTime() + '.png'
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+        '--disable-extensions-except=/path/to/manifest/folder/',
+        '--load-extension=/path/to/manifest/folder/',
+        '--no-sandbox'
+        ]
+    })
     const page = await browser.newPage()
     await page.setViewport({ width: 1000, height: 800 })
-    await page.goto('https://www.ultimate-bravery.net/Classic')
+    await page.goto('https://www.ultimate-bravery.net/Classic', { waitUntil: ['load', 'domcontentloaded'] })
     await page.click('.fc-button.fc-cta-do-not-consent.fc-secondary-button')
     await page.screenshot({ path: filename })
     await browser.close()
@@ -16,5 +23,5 @@ async function getScreenshot() {
 
 (async () => {
     const file = await getScreenshot()
-    fs.unlinkSync(file)
+    // fs.unlinkSync(file)
 })()
